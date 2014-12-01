6a9b0b54392b2063bb9a42becc459c40387ce2cf
Mark Harris <mharris@covermymeds.com>
2014-12-01 17:35:14 -0500

    handle dashboard problems seen in demo

--- app/helpers/pa_requests_helper.rb
+++ app/helpers/pa_requests_helper.rb
@@ -1,8 +1,4 @@
 module PaRequestsHelper
-
-  def request_tokens
-    PaRequest.select('cmm_token').inject([]) {|acc,n| acc.push("'#{n.cmm_token}'")}
-  end
   
   def cmm_outcome_label(outcome)
     lookup = {

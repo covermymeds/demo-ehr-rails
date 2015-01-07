a76f11f8960ab1d900f302e8695c2097de2b56d3
Ryan Bone <rbone@covermymeds.com>
2015-01-07 13:29:34 -0500

    Update user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 describe User, type: :model do
+  it { should respond_to(:first_name) }
+  it { should respond_to(:last_name) }
+  it { should respond_to(:role_id) }
+
   describe "#prescriber?" do
-    let(:user) { User.new name: 'Test McTestface', npi: npi }
-    
+    let(:user) { User.new first_name: 'Test McTestface', npi: npi }
+
     context "when user has a valid NPI number" do
       let(:npi) { "1234512345" }
-      
+
       it "returns true" do
         expect(user).to be_prescriber
       end

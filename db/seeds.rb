85be0d1767d81aa7793d5ddabb72df13483ae2b2
Ryan Bone <rbone@covermymeds.com>
2015-01-07 14:05:41 -0500

    update seeds file with roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,10 @@ Patient.destroy_all
 
 PaRequest.destroy_all
 
+Role.destroy_all
+Role.create! description: 'Doctor'
+Role.create! description: 'Staff'
+
 patients = [
   {first_name:'Autopick', last_name:'Smith',    gender:'f', date_of_birth:'10/01/1971', street_1:'221 Baker St.', street_2:'Apt B', city:'London', state:'OH', zip:'43210', phone_number:'614-555-1212', email:'test@covermymeds.com', bin:'111111', pcn:'SAMP001', group_id:'NOTREAL'},
   {first_name:'Autopick', last_name:'Johnson', gender:'m', date_of_birth:'10/01/1971', street_1:'221 Baker St.', street_2:'Apt B', city:'London', state:'OH', zip:'43210', phone_number:'614-555-1212', email:'test@covermymeds.com', bin:'111111', pcn:'SAMP001', group_id:'NOTREAL'},
@@ -24,7 +28,7 @@ patients = [
 
   ]
 
-patients.each do |patient|	
+patients.each do |patient|
   Patient.create(patient)
 end
 
@@ -45,5 +49,5 @@ end
 
 User.destroy_all
 
-User.new(name: "Dr. Alexander Fleming", npi: "1234567890").tap {|u| u[:id] = 1}.save!
-User.new(name: "Staff", npi: nil).tap {|u| u[:id] = 2}.save!
+User.new(first_name: "Alexander", last_name: "Fleming", npi: "1234567890", role_id: Role.doctor.id).tap {|u| u[:id] = 1}.save!
+User.new(first_name: "Staff", npi: nil, role_id: Role.staff.id).tap {|u| u[:id] = 2}.save!

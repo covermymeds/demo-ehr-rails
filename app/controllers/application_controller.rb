f7e9deefa0594c155f6f957bdec1f9831bbd880a
Ryan Bone <rbone@covermymeds.com>
2015-01-09 10:47:17 -0500

    Move current_user to ApplicationHelper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_use_custom_ui
 
-  def current_user
-    @current_user ||= User.find_by_id(session["user_id"])
-    @current_user
-  end
-  helper_method :current_user
-
-
   def salutation
     if current_user and current_user.role_id == Role.doctor.id
       "Welcome, Dr. #{current_user.last_name}"

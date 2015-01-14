08cf7b1ed2f07eb69fb5758c3fe57bb077ecb3fe
Ryan Bone <ryanzbone@gmail.com>
2015-01-14 10:09:53 -0500

    move 'Failure' status to open_statuses

--- app/assets/javascripts/pa_requests.js
+++ app/assets/javascripts/pa_requests.js
@@ -2,11 +2,10 @@ $(function () {
   var options = window.config;
 
   var new_statuses = ["New"];
-  var open_statuses = ["Appealed", "PA Request", "Shared", "Shared \\ Accessed Online", "Sent to Plan", "Question Request", "Question Response", "Cancel Request Sent" ];
+  var open_statuses = ["Appealed", "PA Request", "Shared", "Shared \\ Accessed Online", "Sent to Plan", "Question Request", "Question Response", "Cancel Request Sent", "Failure"];
   var closed_statuses = ["PA Response", "Provider Cancel", "Expired", "Archived" ];
   var appealed_statuses = ["Appeal Request", "Appeal Response", "Appealed"];
-  var error_statuses = ["Failure"]
-  var all_statuses = new_statuses.concat(open_statuses).concat(closed_statuses).concat(appealed_statuses).concat(error_statuses);
+  var all_statuses = new_statuses.concat(open_statuses).concat(closed_statuses).concat(appealed_statuses);
 
   var dashboard_options = {
     apiId: options.apiId,
@@ -18,14 +17,13 @@ $(function () {
       'Open': { workflow_statuses: open_statuses, data: [] },
       'Closed': { workflow_statuses: closed_statuses, data:[]},
       'Appeal': { workflow_statuses: appealed_statuses, data:[]},
-      'Error': { workflow_statuses: error_statuses, data:[]}
     }
   };
   $('#dashboard').dashboard(dashboard_options);
 
   // drug search for the "new pa" form
   $('#prescription_drug_number').drugSearch(options);
-  
+
   // if we got here from choosing a patient, the drug will already be filled in
   if ($('#prescription_drug_number').val()) {
     $('#pa_request_form_id').formSearch({

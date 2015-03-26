6b20b1becf94c8b17584b519c55c586eb808999b
Ryan Stocker <rstocker@covermymeds.com>
2015-03-26 16:43:01 -0400

    remove unique index constraint on users#first_name since we may have 7 Ryans :)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150311211852) do
+ActiveRecord::Schema.define(version: 20150326200916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,7 +114,6 @@ ActiveRecord::Schema.define(version: 20150311211852) do
     t.integer  "role_id"
   end
 
-  add_index "users", ["first_name"], name: "index_users_on_first_name", unique: true, using: :btree
   add_index "users", ["npi"], name: "index_users_on_npi", unique: true, using: :btree
   add_index "users", ["role_id"], name: "index_users_on_role_id", using: :btree
 

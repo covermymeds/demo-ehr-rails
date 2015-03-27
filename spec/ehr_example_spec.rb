e7b25bdaae2fed5f257f466dffe92b484ddd0a03
Mark Harris <mharris@covermymeds.com>
2015-03-27 18:01:47 -0400

    update tests to match

--- spec/ehr_example_spec.rb
+++ spec/ehr_example_spec.rb
@@ -17,10 +17,6 @@ describe 'eHR Example App' do
       visit '/logout'
     end
 
-    it 'defaults to the custom UI' do
-      expect( find(:css, 'select#change_view').value ).to eq('Custom UI')
-    end
-
     it 'should navigate to the patients view', js: true do
       click_link('Patients')
       expect(page).to have_content('Patients')
@@ -66,14 +62,16 @@ describe 'eHR Example App' do
     context 'Resources' do
       before { click_link ('Resources') }
 
+      it 'defaults to the custom UI' do
+        expect(find_link('Use CoverMyMeds Request Page')[:href]).to match(/toggle_ui/)
+      end
+
       it 'should navigate to the api documentation' do
-        click_link('API Documentation')
-        expect(page).to have_title('API Reference')
+        expect(find_link('API Documentation')[:href]).to match("/api")
       end
 
       it 'should display the source code when asked' do
-        click_link('Source Code')
-        expect(page).to have_content('Reference implementation of an EHR integration with CoverMyMeds, written in Ruby on Rails.')
+        expect(find_link('Source Code')[:href]).to match("/code")
       end
 
       it 'should reset the database when asked', js: true do

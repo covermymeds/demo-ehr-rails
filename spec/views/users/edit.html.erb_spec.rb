16fb41b062824e9bdade9ac62d44043876378302
Ryan Stocker <rstocker@covermymeds.com>
2015-03-30 10:46:26 -0400

    junklet only returning 8 digits again

--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "users/edit", type: :view do
   junklet :first, :last
 
-  let(:npi)    { junk(:int, size: 10) }
+  let(:npi)    { rand.to_s[2..11] } # junk(:int, size: 10) was returning 8 digits
   let!(:user)  { User.create!(first_name: first, last_name: last, role_id: 1, npi: npi ) }
   let!(:roles) { [Role.create(description: 'Doctor')] }
 

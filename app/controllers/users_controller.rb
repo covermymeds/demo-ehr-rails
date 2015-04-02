0a3a64ce1f0be87c925ba79a8b5b55e6e1ae8116
Ryan Stocker + Rachel Twyford <rstocker+rs+rt@covermymeds.com>
2015-04-02 14:21:56 -0400

    fix registered_with_cmm checkbox

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :npi, :role_id)
+      params.require(:user).permit(:first_name, :last_name, :npi, :fax, :registered_with_cmm, :role_id)
     end
 
     def login_with_id!
--- spec/features/credential_registration_spec.rb
+++ spec/features/credential_registration_spec.rb
@@ -12,12 +12,15 @@ describe 'Registering Credentials with CMM' do
   end
 
   context 'from the user edit page' do
-    it 'has the amazing checkbox' do
+    it 'allows the prescriber to register their credentials' do
       visit edit_user_path(user)
       fill_in('Npi', with: '1234567890')
       fill_in('Fax', with: '1-800-555-5555')
       check('Register with CMM')
       click_button('Update User')
+
+      visit edit_user_path(user)
+      expect(page).to have_checked_field('Register with CMM', disabled: true)
     end
   end
 end

0cccf1abe45dcc85f86fc5419d880c77889649dd
ksearfos <ksearfos@covermymeds.com>
2015-04-09 09:53:51 -0400

    have to re-migrate after brining in latest master

--- db/schema.rb
+++ db/schema.rb
@@ -98,13 +98,6 @@ ActiveRecord::Schema.define(version: 20150326200916) do
     t.string "description", null: false
   end
 
-  create_table "standard_xmls", force: true do |t|
-    t.string   "filename"
-    t.string   "type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "npi"

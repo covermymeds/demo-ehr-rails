19b9ef1b991225bcb723c1777cb70d107d0dad8c
Robert Lude <rlude@covermymeds.com>
2015-04-15 13:14:44 -0400

    update cmm_callback spec comment

--- spec/controllers/cmm_callbacks_controller_spec.rb
+++ spec/controllers/cmm_callbacks_controller_spec.rb
@@ -18,8 +18,7 @@ RSpec.describe CmmCallbacksController, type: :controller do
       JSON.parse(File.read("spec/fixtures/delete_request.json"))
     end
 
-    # in both of these tests (adds.. and deletes...) the controller redirects to show the
-    # request that was created by the callback, or that was updated by the callback.
+    # in both of these tests (adds.. and deletes...) the controller responds with 200
     it "adds a request when prompted from the callback" do
       post :create, valid_request, format: :json
       expect(response.status).to eq(200)

bb1b2f548c22d40e6282a8cd8ee83837220bbdf2
Ryan Stocker + Rachel Twyford <rtwyford+rs+rt@covermymeds.com>
2015-04-15 12:14:53 -0500

    kill off unnecessary instance var

--- app/controllers/cmm_callbacks_controller.rb
+++ app/controllers/cmm_callbacks_controller.rb
@@ -29,19 +29,16 @@ class CmmCallbacksController < ApplicationController
   # changes. In that case, we need to update the PA record in our system with the
   # new values in the callback.
   def create
-    # pull the pa request out of the JSON
-    @request = request_params
-
     # create a callback object to log that we received this callback
-    @callback = CmmCallback.new content: @request.to_json
+    @callback = CmmCallback.new content: request_params.to_json
 
     # see if the PA exists already in our local database
-    @pa = PaRequest.find_by_cmm_id(@request['id'])
+    @pa = PaRequest.find_by_cmm_id(request_params['id'])
 
-    users = User.where(npi: @request['prescriber']['npi'])
+    users = User.where(npi: request_params['prescriber']['npi'])
     # see if we have the prescriptions in the EHR
-    found_prescription = Prescription.where(drug_number: @request['prescription']['drug_id']).any?
-    found_npi = User.where(npi: @request['prescriber']['npi']).any?
+    found_prescription = Prescription.where(drug_number: request_params['prescription']['drug_id']).any?
+    found_npi = User.where(npi: request_params['prescriber']['npi']).any?
     found_pa = @pa.present?
 
     if !found_npi
@@ -58,17 +55,17 @@ class CmmCallbacksController < ApplicationController
     if !found_pa
       # couldn't find it in our db, must be a retrospective
       @pa = PaRequest.new
-      @pa.init_from_callback(@request)
+      @pa.init_from_callback(request_params)
     else
       # if we have a record of the PA, delete it if appropriate
-      if is_delete?(@request)
+      if is_delete?(request_params)
         @pa.update_attributes(cmm_token: nil)
         users.each do |u|
           u.alerts.create(message: "A PA was deleted.")
         end
       else
         # if it's not a delete, then it's an update
-        @pa.update_from_callback(@request)
+        @pa.update_from_callback(request_params)
         users.each do |u|
           u.alerts.create(message: "A PA was updated")
         end

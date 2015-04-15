687243103c096669b333520f7aefdbce209ab82b
Ryan Stocker + Rachel Twyford <rtwyford+rs+rt@covermymeds.com>
2015-04-15 16:00:09 -0500

    WIP for cmm_callbacks_controller#create

--- app/controllers/cmm_callbacks_controller.rb
+++ app/controllers/cmm_callbacks_controller.rb
@@ -32,30 +32,28 @@ class CmmCallbacksController < ApplicationController
     # create a callback object to log that we received this callback
     callback = CmmCallback.new content: request_params.to_json
 
-    # see if the PA exists already in our local database
-    pa = PaRequest.find_by_cmm_id(request_params['id'])
+    handler = PaHandler.new(cmm_id: request_params['id'], npi: request_params['prescriber']['npi'], drug_number: request_params['prescription']['drug_id'])
 
     users = User.where(npi: request_params['prescriber']['npi'])
     # see if we have the prescriptions in the EHR
     found_prescription = Prescription.where(drug_number: request_params['prescription']['drug_id']).any?
-    found_npi = User.where(npi: request_params['prescriber']['npi']).any?
+    pa = PaRequest.find_by_cmm_id(request_params['id'])
     found_pa = pa.present?
 
-    render(status: 410, text: 'NPI not found') and return unless found_npi
-
-    if found_npi && !found_prescription
+    case handler.brussel_sprouts.status
+    when :npi_not_found
+      render(status: 410, text: 'NPI not found') and return
+    when :prescription_not_found
       users.each do |u|
         u.alerts.create(message: "Your NPI was found, but the prescription didn't match")
       end
       render(status: 404, text: 'prescription not found') and return
-    end
-
-    if !found_pa
+    when :new_retrospective
       # couldn't find it in our db, must be a retrospective
       pa = PaRequest.new
       pa.init_from_callback(request_params)
-    else
-      # if we have a record of the PA, delete it if appropriate
+    when :pa_found
+  # if we have a record of the PA, delete it if appropriate
       if is_delete_request?(request_params)
         pa.update_attributes(cmm_token: nil)
         users.each do |u|
@@ -68,6 +66,7 @@ class CmmCallbacksController < ApplicationController
           u.alerts.create(message: 'A PA was updated')
         end
       end
+
     end
 
     # save our updated PA, and keep track of the callback that spawned it
---
+++ app/models/pa_handler.rb
@@ -0,0 +1,35 @@
+class PaHandler
+
+  def initialize(cmm_id: cmm_id, npi: npi, drug_number: drug_number)
+    @npi = npi
+    # see if the PA exists already in our local database
+    @pa = PaRequest.find_by_cmm_id(cmm_id)
+    @drug_number = drug_number
+  end
+
+  def pa_status(status)
+    OpenStruct.new(status: status)
+  end
+
+  def brussel_sprouts
+    if !npi_found?
+      pa_status(:npi_not_found)
+    elsif !found_prescription?
+      pa_status(:prescription_not_found)
+    elsif !@pa.present?
+      pa_status(:new_retrospective)
+    else
+      pa_status(:pa_found)
+    end
+  end
+
+  private
+  def npi_found?
+    User.where(npi: @npi).any?
+  end
+
+  def found_prescription?
+    npi_found? && Prescription.where(drug_number: @drug_number).any?
+  end
+
+end

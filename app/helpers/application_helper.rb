9a222403b5d19c1567f2c2febf25ba95e6dc18ec
Ryan Stocker + Rachel Twyford <rstocker+rs+rt@covermymeds.com>
2015-04-16 15:55:20 -0400

    pluralize_without_count application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,4 +39,9 @@ module ApplicationHelper
     JSON.pretty_generate(JSON.parse(json))
   end
 
+  def pluralize_without_count(count, noun, text = nil)
+    if count != 0
+      count == 1 ? "#{noun}#{text}" : "#{noun.pluralize}#{text}"
+    end
+  end
 end

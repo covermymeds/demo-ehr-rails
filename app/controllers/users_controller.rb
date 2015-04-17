387b129f68fabc9795ed871e3cc32405195a5276
Ryan Stocker + Rachel Twyford <rtwyford+rs+rt@covermymeds.com>
2015-04-17 14:11:44 -0500

    rubocop cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   end
 
   def logout
-    session["user_id"] = nil
+    session['user_id'] = nil
     @current_user = nil
     redirect_to root_url
   end
@@ -26,10 +26,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       register = !@user.registered_with_cmm? && params[:user][:registered_with_cmm] == '1'
       if @user.update(user_params)
-        if register
-          client = ApiClientFactory.build(use_integration: !!session[:use_integration])
-          client.create_credential(@user.npi, @user.fax)
-        end
+        create_registration! if register
         format.html { redirect_to root_url, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
@@ -47,44 +44,50 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    def set_roles
-      @roles = Role.all
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :npi, :fax, :registered_with_cmm, :role_id)
-    end
+  def set_roles
+    @roles = Role.all
+  end
 
-    def login_with_id!
-      user = User.find_by_id(params[:id])
-      if user && login_user!(user)
-        flash_message("Logged in as #{user.display_name}")
-        redirect_to home_url
-      else
-        flash_message("Login failed.  User not found")
-        redirect_to home_url
-      end
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :npi, :fax, :registered_with_cmm, :role_id)
+  end
 
-    def login_with_role!
-      user = User.joins(:role).where("roles.description = ?", params[:role_description]).first
-      if user && login_user!(user)
-        flash_message("Logged in as #{user.display_name}")
-        redirect_to home_url
-      else
-        flash_message("Demo account not found.  Try resetting the database.")
-        redirect_to home_url
-      end
+  def login_with_id!
+    user = User.find_by_id(params[:id])
+    if user && login_user!(user)
+      flash_message("Logged in as #{user.display_name}")
+      redirect_to home_url
+    else
+      flash_message('Login failed.  User not found')
+      redirect_to home_url
     end
+  end
 
-    def login_user!(user)
-      session["user_id"] = user.id
-      @current_user ||= user
+  def login_with_role!
+    user = User.joins(:role).where('roles.description = ?', params[:role_description]).first
+    if user && login_user!(user)
+      flash_message("Logged in as #{user.display_name}")
+      redirect_to home_url
+    else
+      flash_message('Demo account not found.  Try resetting the database.')
+      redirect_to home_url
     end
+  end
+
+  def login_user!(user)
+    session['user_id'] = user.id
+    @current_user ||= user
+  end
+
+  def create_registration!
+    client = ApiClientFactory.build(use_integration: !!session[:use_integration])
+    client.create_credential(@user.npi, @user.fax)
+  end
 end

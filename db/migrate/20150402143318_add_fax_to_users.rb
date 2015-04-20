3285c01cc644109ca49ff6a7f8f8d6985a2593f1
Ryan Stocker + Rachel Twyford <rstocker+rs+rt@covermymeds.com>
2015-04-20 10:27:27 -0400

    remove credentials fields in User model

--- db/migrate/20150402143318_add_fax_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddFaxToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :fax, :string
-  end
-end
--- db/migrate/20150402172928_add_registed_with_cmm_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddRegistedWithCmmToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :registered_with_cmm, :boolean
-  end
-end

89e132aea388da07965a46a4f8fa452f1a779e7f
Ryan Stocker + Rachel Twyford <rtwyford+rs+rt@covermymeds.com>
2015-05-20 10:11:02 -0500

    fix prescriptions form for pa_required is disabled

--- app/assets/javascripts/pa_requests.js
+++ app/assets/javascripts/pa_requests.js
@@ -63,7 +63,8 @@ $(function () {
             } else {
               $('#pa_required_alert').addClass('hidden');
             }
-            $('#prescription_pa_required').prop('checked', data.prescriptions[0].pa_required).prop('disabled', data.prescriptions[0].autostart);
+            $('#prescription_pa_required').val(data.prescriptions[0].pa_required ? '1' : '0');
+            $('#display_pa_required').prop('checked', data.prescriptions[0].pa_required).prop('disabled', data.prescriptions[0].autostart);
           }
         });
       }

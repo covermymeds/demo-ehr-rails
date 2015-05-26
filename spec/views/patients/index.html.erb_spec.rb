5c8eb73524c64975e124cd68ffd29e21c16d8ff9
Ryan Stocker + Rachel Twyford <rstocker+rs+rt@covermymeds.com>
2015-05-26 15:40:03 -0400

    Fix view specs

--- spec/views/patients/index.html.erb_spec.rb
+++ spec/views/patients/index.html.erb_spec.rb
@@ -2,20 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "patients/index", type: :view do
   before(:each) do
-    assign(:patients, [
-      Patient.create!(
-        first_name: "First Name",
-        last_name: "Last Name",
-        date_of_birth: "10/11/1971",
-        state: "OH"
-      ),
-      Patient.create!(
-        first_name: "First Name",
-        last_name: "Last Name",
-        date_of_birth: "10/11/1971",
-        state: "OH"
-      )
-    ])
+    Patient.create!(
+      first_name: "First Name",
+      last_name: "Last Name",
+      date_of_birth: "10/11/1971",
+      state: "OH"
+    )
+    Patient.create!(
+      first_name: "First Name",
+      last_name: "Last Name",
+      date_of_birth: "10/11/1971",
+      state: "OH"
+    )
+    assign(:patients, Patient.all.page(1))
   end
 
   it "renders a list of patients" do

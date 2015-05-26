6e21c399685033ca2dc49146c501b05d5938899e
Ryan Stocker + Rachel Twyford <rstocker+rs+rt@covermymeds.com>
2015-05-26 14:18:44 -0400

    paginate patients

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -4,7 +4,7 @@ class PatientsController < ApplicationController
   # GET /patients
   # GET /patients.json
   def index
-    @patients = Patient.all
+    @patients = Patient.page(params[:page])
   end
 
   # GET /patients/1

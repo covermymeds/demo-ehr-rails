e312f960aeb6bba4d9468677d40f172ec8d25d91
Mark Harris <mharris@covermymeds.com>
2016-01-26 00:56:36 -0500

    cleanup

--- spec/models/prescription_spec.rb
+++ spec/models/prescription_spec.rb
@@ -11,18 +11,14 @@ RSpec.describe Prescription, type: :model do
       drug_name: 'My Drug'
     }
   end
+
   it 'allows a pharmacy' do
-    # setup
     prescription = Prescription.new(prescription_params)
     prescription.patient = Patient.new(first_name: 'john', last_name: 'doe', state: 'OH', date_of_birth: '01/01/1970')
     prescription.pharmacy = Pharmacy.new(name: 'this is a pharmacy')
 
-    # exercise
     prescription.save
 
-    # verify
     expect(prescription).to be_valid
-
-    # teardown is handled for you by RSpec
   end
 end

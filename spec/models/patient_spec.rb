2fa3437ff6170044c9db1d8520c6e5075eaff2a8
Mark Harris <mharris@covermymeds.com>
2016-01-26 00:52:30 -0500

    update patient spec

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -2,58 +2,60 @@ require 'rails_helper'
 
 RSpec.describe Patient, type: :model do
   # Prefix class methods with a '.'
+  let(:first) { 'john' }
+  let(:last)  { 'doe' }
+  let(:state) { 'OH' }
+  let(:date_of_birth) { '01/01/1970' }
+  let(:full_name) { "#{first} #{last}" }
+  let(:description) { "#{full_name} #{date_of_birth} #{state}" }
+  let(:patient) { Patient.new(first_name: first, last_name: last, 
+                    state: state, date_of_birth: date_of_birth)}
+
   it 'allows first and last name, state, and date_of_birth' do
-    # setup
-    patient = Patient.new(first_name:'john', last_name:'doe', state:'OH', date_of_birth:'01/01/1970')
-
-    # exercise
-    patient.save
-
     # verify
     expect(patient).to be_valid
-
-    # teardown is handled for you by RSpec
   end
 
-  it 'requires first name' do
-    patient = Patient.new(last_name:'doe', state:'OH', date_of_birth:'01/01/1970')
-    patient.save
-    expect(patient).not_to be_valid
+  describe 'with nil first name' do
+    let(:first) { nil }
+    it 'signals error' do
+      expect(patient).not_to be_valid
+    end
   end
 
-  it 'requires last name' do
-    patient = Patient.new(first_name:'john', state:'OH', date_of_birth:'01/01/1970')
-    patient.save
-    expect(patient).not_to be_valid
+  describe 'with nil last name' do 
+    let(:last) { nil }
+    it 'fails to create PA' do
+      expect(patient).not_to be_valid
+    end
   end
 
-  it 'requires state' do 
-    patient = Patient.new(first_name:'john', last_name:'doe', date_of_birth:'01/01/1970')
-    patient.save
-    expect(patient).not_to be_valid
+  describe 'with nil state' do 
+    let(:state) { nil }
+    it 'fails to create PA' do 
+      expect(patient).not_to be_valid
+    end
   end
 
-  it 'requires date of birth' do
-    patient = Patient.new(first_name:'john', last_name:'doe', state:'OH')
-    patient.save
-    expect(patient).not_to be_valid
+  describe 'with nil date_of_birth' do
+    let(:date_of_birth) { nil }
+    it 'requires date of birth' do
+      expect(patient).not_to be_valid
+    end
   end
 
-  it 'requires american format dob' do
-    patient = Patient.new(first_name:'john', last_name:'doe', state:'OH', date_of_birth:'July 4, 1776')
-    patient.save
-    expect(patient).not_to be_valid
+  describe 'with wrong format date_of_birth' do
+    let(:date_of_birth) { 'July 4, 1776' }
+    it 'requires american format dob' do
+      expect(patient).not_to be_valid
+    end
   end
 
-  it 'returns full name' do
-    patient = Patient.new(first_name:'john', last_name:'doe', state:'OH', date_of_birth:'01/01/1970')
-    full_name = patient.full_name
-    expect(full_name).to eql('john doe') 
+  it 'provides correct full name' do
+    expect(patient.full_name).to eql(full_name) 
   end
 
-  it 'returns description' do
-    patient = Patient.new(first_name:'john', last_name:'doe', state:'OH', date_of_birth:'01/01/1970')
-    description = patient.description
-    expect(description).to eql('john doe 01/01/1970 OH')
+  it 'provides a good description' do
+    expect(patient.description).to eql(description)
   end
 end

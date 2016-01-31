19e80661af68cc1664f6e1ecf26dab184d709f86
Mark Harris <mharris@covermymeds.com>
2016-01-31 14:32:50 -0500

    refactor formulary

--- app/controllers/formularies_controller.rb
+++ app/controllers/formularies_controller.rb
@@ -2,23 +2,11 @@ class FormulariesController < ApplicationController
   
   def pa_required
     patient = Patient.find(params[:patient_id])
-    
-    result = CoverMyMeds.default_client.post_indicators(
-      prescription: { drug_id: params[:drug_id], name: params[:drug_name] }, 
-      patient: patient.to_patient_hash, 
-      payer: patient.to_payer_hash)
-
-    # for purposes of demonstration, vanilla flavors require PA
-    # while chocolate flavors are auto-started and require Patient
-    rx = result['indicator']['prescription']
-    drug_name = rx['name'].downcase
-    rx['pa_required'] ||= (drug_name.include?("vanilla") || drug_name.include?("chocolate"))
-    rx['autostart'] ||= drug_name.include?("chocolate")
+    prescription = Prescription.new(drug_number: params[:drug_id],
+                                    drug_name: params[:drug_name])
+    result = Formulary.pa_required?(patient, prescription)
 
     render json: result
   end
-
-private
-
 
 end
--- spec/models/formulary_spec.rb
+++ spec/models/formulary_spec.rb
@@ -3,16 +3,75 @@ require 'rails_helper'
 describe Formulary do
 
   describe '.pa_required?' do
-    let (:drug_name) { SecureRandom.uuid }
-    let (:banana)    { 'banana' }
-    let (:prescriptions) { [
-      { name: drug_name },
-      { name: banana },
-    ] }
-
-    it 'returns true for bananas and false for not bananas' do
-      expect(described_class.pa_required?(prescriptions)).to include({name: banana, autostart: true}, {name: drug_name, autostart: false})
+    let(:result) { described_class.pa_required?(patient, drug) }
+    let(:drug_number) { junk(:int, size: 6, format: :string) }
+    let(:drug_name) { junk }
+    let(:ndc) { junk(:int, size: 11, format: :string) }
+    let(:prescription_params) do
+      {
+        drug_number: drug_number,
+        drug_name: drug_name
+      }
     end
-  end
+    let(:drug) { Prescription.new(prescription_params) }
 
+    let(:patient_params) do 
+      {
+        first_name: junk,
+        last_name: junk,
+        date_of_birth: '10/01/1971',
+        state: ['OH', 'IN', 'KY', 'MA', 'ME', 'WA'].sample,
+        bin: '773836',
+        pcn: 'MOCKPBM',
+        group_id: 'ABC1'
+      }
+    end
+    let(:patient) { Patient.new(patient_params) }
+    let(:result_params) do
+      {
+        indicator: {
+          prescription: {
+            drug_id: drug_number,
+            name: drug_name,
+            autostart: false,
+            pa_required: false,
+            predicted: false,
+          }
+        }
+      }
+    end
+    before(:each) do 
+      stub_request(:post, "https://#{ENV['CMM_API_KEY']}:#{ENV['CMM_API_SECRET']}@api.covermymeds.com/indicators/?v=1")
+             .to_return(status: 200, body: result_params.to_json, :headers => {})
+    end
+
+    context 'when the drug is chocolate flavored' do
+      let(:drug_name) { 'Chocolate flavor' }
+
+      it 'returns pa:true/autostart:true' do
+        rx = result[:indicator][:prescription]
+        expect(rx[:pa_required]).to eq(true)
+        expect(rx[:autostart]).to eq(true)
+      end
+    end
+
+    context 'when the drug is vanilla flavored' do
+      let(:drug_name) { 'Vanilla flavor' }
+
+      it 'returns pa:true/autostart:false' do
+        rx = result[:indicator][:prescription]
+          expect(rx[:pa_required]).to eq(true)
+          expect(rx[:autostart]).to eq(false)
+      end
+    end
+
+    context 'when the drug is an arbitrary drug' do
+      it 'returns pa:false/autostart:false' do
+        rx = result[:indicator][:prescription]
+        expect(rx[:pa_required]).to eq(false)
+        expect(rx[:autostart]).to eq(false)
+      end        
+    end
+
+  end
 end

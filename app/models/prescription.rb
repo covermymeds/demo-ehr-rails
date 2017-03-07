36cd63f64be3a5e06d9f2256fad07ddcabb889b8
Mark Harris <mark@isandlot.com>
2017-03-07 11:43:52 -0500

    Fix no pa showing on prescription (#4)
    * Change pa prescription page (#109)
    
    * simplify PA list view on patient prescription page
    
    * not crash when deleting prescriptions
    
    * show correct number of pa each patient in list
    
    * require PA for the prescriptions seeded in db reset action
    
    * address pa not showing on patient show page
    
    * CMM_API_ID => _KEY
    
    * some rubocop fixes
    
    * silence rubocop warnings
    
    * clean up prescription model for rubocop

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -3,7 +3,9 @@ class Prescription < ActiveRecord::Base
   belongs_to :pharmacy, inverse_of: :prescriptions
   has_many :pa_requests, dependent: :destroy, inverse_of: :prescription
 
-  validates :drug_number, format: {with: /[0-9]+/ , message: 'Drug Number is invalid'}
+  validates :drug_number, format: {
+    with: /[0-9]+/, message: 'Drug number must be a DDID or NDC'
+  }
   validates_presence_of :patient
 
   default_scope { order(date_prescribed: :desc) }
@@ -11,61 +13,55 @@ class Prescription < ActiveRecord::Base
   scope :active, -> { where(active: true) }
 
   FREQUENCIES = [
-    ['qD - EVERY DAY', 'qD'],
+    ['qD - EVERY DAY',    'qD'],
     ['BID - TWICE A DAY', 'BID'],
     ['TID - THREE A DAY', 'TID'],
-    ['QID - FOUR A DAY', 'QID'],
-    ['PRN - AS NEEDED', 'PRN'],
-    ['UD - AS DIRECTED', 'UD']
+    ['QID - FOUR A DAY',  'QID'],
+    ['PRN - AS NEEDED',   'PRN'],
+    ['UD - AS DIRECTED',  'UD']
   ].freeze
 
-  def initiate_pa current_user
-    pa_request = pa_requests.build(
-      user: current_user,
-      state: patient.state,
-      urgent: false)
+  PERDAY = {
+    qd:  1,
+    bid: 2,
+    tid: 3,
+    qid: 4
+  }.freeze
 
-    begin
-      response = CoverMyMeds.default_client.create_request RequestConfigurator.new(pa_request).request
 
-      pa_request.set_cmm_values(response)
-      pa_request.save
-    rescue CoverMyMeds::Error::HTTPError => e
-      false
-    end
+  def initiate_pa(current_user)
+    pa_request = pa_requests.build(user: current_user,
+                                   state: patient.state,
+                                   urgent: false)
+    response = CoverMyMeds
+               .default_client
+               .create_request RequestConfigurator.new(pa_request).request
+
+    pa_request.set_cmm_values(response).save
+  rescue CoverMyMeds::Error::HTTPError => e
+    logger.error("Unable to create PA request: #{e.message}")
+    false
   end
 
   def days_supply
-    case frequency
-    when 'qD'
-      quantity
-    when 'BID'
-      (quantity / 2).ceil
-    when 'TID'
-      (quantity / 3).ceil
-    when 'QID'
-      (quantity / 4).ceil
-    else 
-      quantity
-    end
+    return quantity if frequency.nil?
+    per_day = PERDAY.fetch(frequency.downcase.to_sym, 1)
+    (quantity / per_day)
   end
 
   def quantity_unit_of_measure
-    "C48480" # Capsule    
+    'C48480' # Capsule from Quantity Unit Of Measure NCPDP codes
   end
 
   def diagnosis9
-    "800.14"
+    '800.14'
   end
 
   def diagnosis10
-    "K80.67"
+    'K80.67'
   end
 
   def script
-    "#{drug_name} #{frequency}, Quantity: #{quantity}, Refills: #{refills.to_s}"
+    "#{drug_name} #{frequency}, Quantity: #{quantity}, Refills: #{refills}"
   end
-
-  private
-
 end
